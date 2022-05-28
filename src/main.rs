c47e4e3262464d105c1da708081487b5a7f63738
Erhune <erhune@npng.org>
2022-05-28 20:13:16 +0900

    Implement logic for MOSFET control

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@
 
 use avr_device::atmega328p::Peripherals;
 use avr_hal_generic::delay::Delay;
-use embedded_hal::prelude::_embedded_hal_blocking_delay_DelayMs;
 
 use panic_halt as _;
 
@@ -13,75 +12,70 @@ type Clock = atmega_hal::clock::MHz8;
 fn main() -> ! {
     let dp = Peripherals::take().unwrap();
     let pins = atmega_hal::pins!(dp);
-    // let mut delay = Delay::<Clock>::new();
+    let mut _delay = Delay::<Clock>::new();
 
     let buttons = [
         pins.pc0.into_pull_up_input().downgrade(),
         pins.pc1.into_pull_up_input().downgrade(),
         pins.pc2.into_pull_up_input().downgrade(),
         pins.pc3.into_pull_up_input().downgrade(),
-        pins.pc4.into_pull_up_input().downgrade(),
-        pins.pc5.into_pull_up_input().downgrade(),
-        pins.pc6.into_pull_up_input().downgrade(),
     ];
     let mut leds = [
         pins.pb0.into_output().downgrade(),
         pins.pb1.into_output().downgrade(),
         pins.pb2.into_output().downgrade(),
         pins.pb3.into_output().downgrade(),
-        pins.pb4.into_output().downgrade(),
-        pins.pb5.into_output().downgrade(),
-        pins.pb6.into_output().downgrade(),
     ];
-    let mut opendrain = [
-        pins.pd0.into_opendrain().downgrade(),
-        pins.pd1.into_opendrain().downgrade(),
-        pins.pd2.into_opendrain().downgrade(),
-        pins.pd3.into_opendrain().downgrade(),
-        pins.pd4.into_opendrain().downgrade(),
-        pins.pd5.into_opendrain().downgrade(),
-        pins.pd6.into_opendrain().downgrade(),
-        pins.pd7.into_opendrain().downgrade(),
+    let mut mosfets = [
+        pins.pd0.into_output().downgrade(),
+        pins.pd1.into_output().downgrade(),
+        pins.pd2.into_output().downgrade(),
     ];
-    let mut current_button_pressed = [false; 8];
-    let mut current_output = [false; 8];
-
-    // loop {
-    //     for idx in 0..8 {
-    //         if idx % 2 == 0 {
-    //             leds[idx].set_low();
-    //         } else {
-    //             leds[idx].set_high();
-    //         }
-    //     }
-    //     delay.delay_ms(500u16);
-    //     for idx in 0..8 {
-    //         if idx % 2 == 1 {
-    //             leds[idx].set_low();
-    //         } else {
-    //             leds[idx].set_high();
-    //         }
-    //     }
-    //     delay.delay_ms(500u16);
-    // }
+    let mut buttons_pressed = [false; 4];
+    let mut selected_output = 0;
 
     loop {
-        for idx in 0..7 {
+        // Button presses can change the selected output
+        for idx in 0..4 {
             let button_pressed = buttons[idx].is_low();
-            if button_pressed != current_button_pressed[idx] {
-                if button_pressed {
-                    // just pressed
-                    current_output[idx] = !current_output[idx];
-                }
-                current_button_pressed[idx] = button_pressed;
+            if button_pressed && !buttons_pressed[idx] {
+                selected_output = idx;
             }
-            if current_output[idx] {
+            buttons_pressed[idx] = button_pressed;
+        }
+
+        // Reflect selected output on LEDs
+        for idx in 0..4 {
+            if selected_output == idx {
                 leds[idx].set_low();
-                opendrain[idx].set_low();
             } else {
                 leds[idx].set_high();
-                opendrain[idx].set_high();
             }
+        }
+
+        // Reflect selected output on MOSFETs
+        match selected_output {
+            0 => {
+                mosfets[0].set_low();
+                mosfets[1].set_low();
+                mosfets[2].set_low();
+            }
+            1 => {
+                mosfets[0].set_high();
+                mosfets[1].set_low();
+                mosfets[2].set_low();
+            }
+            2 => {
+                mosfets[0].set_low();
+                mosfets[1].set_high();
+                mosfets[2].set_low();
+            }
+            3 => {
+                mosfets[0].set_low();
+                mosfets[1].set_high();
+                mosfets[2].set_high();
+            }
+            _other => unreachable!(),
         }
     }
 }
